e96481b0d9985e510a66cf950aafea2fad2e6e18
Mark Forster <mark@nyqst.ai>
2026-03-10 10:10:14 +0000

    fix(infra): Add missing githubAuthApiRef import to App.tsx

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -32,6 +32,7 @@ import {
 } from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
